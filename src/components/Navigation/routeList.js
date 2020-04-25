171c1828237e30346209d10649af5c0a796cb0b8
Shivam Kejriwal <shivamkejriwal@gmail.com>
2020-04-25 15:30:31 -0700

    feat: log sidebar change

--- src/components/Navigation/routeList.js
+++ src/components/Navigation/routeList.js
@@ -9,35 +9,38 @@ import BusinessIcon from '@material-ui/icons/Business';
 import ContactsIcon from '@material-ui/icons/Contacts';
 
 import * as ROUTES from '../../constants/routes';
+import { Logger } from '../../utils/logger';
 
-
-const SidebarList = () => (
-    <List>
-        <Link to={ROUTES.HOME}>
-            <ListItem button>
-                <StyledListItemIcon>
-                    <HomeIcon />
-                </StyledListItemIcon>
-                <StyledListItemText primary="Home" />
-            </ListItem>
-        </Link>
-        <Link to={ROUTES.CONTACTUS}>
-            <ListItem button>
-                <StyledListItemIcon>
-                    <ContactsIcon />
-                </StyledListItemIcon>
-                <StyledListItemText primary="Contact Us" />
-            </ListItem>
-        </Link>
-        <Link to={ROUTES.SERVICES}>
-            <ListItem button>
-                <StyledListItemIcon>
-                    <BusinessIcon />
-                </StyledListItemIcon>
-                <StyledListItemText primary="Services" />
-            </ListItem>
-        </Link>
-    </List>
-);
+const SidebarList = () => {
+    Logger().log('SidebarList-change');
+    return(
+        <List>
+            <Link to={ROUTES.HOME}>
+                <ListItem button>
+                    <StyledListItemIcon>
+                        <HomeIcon />
+                    </StyledListItemIcon>
+                    <StyledListItemText primary="Home" />
+                </ListItem>
+            </Link>
+            <Link to={ROUTES.CONTACTUS}>
+                <ListItem button>
+                    <StyledListItemIcon>
+                        <ContactsIcon />
+                    </StyledListItemIcon>
+                    <StyledListItemText primary="Contact Us" />
+                </ListItem>
+            </Link>
+            <Link to={ROUTES.SERVICES}>
+                <ListItem button>
+                    <StyledListItemIcon>
+                        <BusinessIcon />
+                    </StyledListItemIcon>
+                    <StyledListItemText primary="Services" />
+                </ListItem>
+            </Link>
+        </List>
+    )
+};
 
 export default SidebarList;
